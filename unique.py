d40f2126d1aa0dd5f42d7ac63d7a4f4c21f74026
Antonio Lima <anto87@gmail.com>
2012-06-04 01:14:59 +0100

    Added script unique.py
    The script unique.py processes the stdin and prints out to the stdout only
    the lines which have not been encountered before (unique lines).

---
+++ unique.py
@@ -0,0 +1,33 @@
+import functools
+
+def iterunique(sequence):
+    """Yield elements not encountered before"""
+    known_elements = set()
+    for element in sequence:
+        if element in known_elements:
+            pass
+        else:
+            known_elements.add(element)
+            yield element
+
+def unique_stdin_stdout():
+    """Print to stdout lines in stdin not encountered before"""
+    try:
+        iterable = iter(functools.partial(raw_input), None)
+        for unique_element in iterunique(iterable):
+            print unique_element
+    except EOFError:
+        pass
+
+
+def cli():
+    try:
+        import argparse
+        parser = argparse.ArgumentParser(description="Print unique lines.")
+        prova = parser.parse_args()
+    except ImportError:
+        pass
+
+if __name__ == "__main__":
+    cli()
+    unique_stdin_stdout()
